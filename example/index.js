ac2605d753de15e24d66a627ae89e30a3db79815
hizlle <hizlle@mail.ru>
2022-12-12 14:16:51 +0600

    example complete

---
+++ example/index.js
@@ -0,0 +1,36 @@
+
+//состояние второй функции
+let selectedTd;
+//получаем с чем будем работать
+let table = document.getElementById('months')
+
+table.onclick = function(event){   
+    let target = event.target;
+    //console.log(target.tagName);
+    //console.log(target.parentNode.tagName);
+    //console.log(this);
+
+    //событие в этом контексте this
+    while(target != this){
+        //если событие контекста this == "TD"
+        console.log(target.parentNode);
+        if(target.tagName == 'TD'){
+            taglighting(target)
+            return
+        }
+        //иначе присваиваем родительскую ноду TR
+        target = target.parentNode
+    }
+
+}
+
+function taglighting(node){
+    //если элемент != undefined 
+    if(selectedTd){
+        selectedTd.classList.remove('highlight')
+    }
+
+    //иначе добавляем из css
+    selectedTd = node
+    selectedTd.classList.add('highlight');
+}
